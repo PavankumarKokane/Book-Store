a90703a21e07b9f8a7d42826e2b7a04c324466bf
PavankumarKokane <pavankokane2013@gmail.com>
2025-02-25 23:18:32 +0530

    Hide search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,13 +13,13 @@ const Header = () => {
               <img loading="eager" src={Logo} width={200} height={92} alt="Logo" />
             </Link>
           </div>
-          <div className="nav-links">
+          {/* <div className="nav-links">
             <ul>
               <li>
                 <Link to="/search">Search By Name</Link>
               </li>
             </ul>
-          </div>
+          </div> */}
         </nav>
       </div>
     </header>
